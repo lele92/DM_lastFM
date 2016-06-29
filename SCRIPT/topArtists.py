d963067c4dd2617f73289016b5c92b636cd2e379
trappola_7 <matteo.borghi20@gmail.com>
2016-06-29 17:43:46 +0200

    - add unique user count function for every artist

--- SCRIPT/topArtists.py
+++ SCRIPT/topArtists.py
@@ -1,12 +1,35 @@
 import pandas as pd
+import numpy as np
 import sys
 
-df = pd.read_csv("../OUTPUT/listenings_genre_merged.csv", skipinitialspace=True, delimiter=",", error_bad_lines=False)
-artist_count = df['artist'].value_counts()[:150]
-out_file = open("../OUTPUT/top_artists.csv", "w")
+df = pd.read_csv("../OUTPUT/prova.csv", skipinitialspace=True, delimiter=",", error_bad_lines=False)
+# df = pd.read_csv("../OUTPUT/listenings_genre_merged.csv", skipinitialspace=True, delimiter=",", error_bad_lines=False)
+groupby_artisti = df.groupby(df['artist'])
+groupby_artisti = sorted(groupby_artisti, key=lambda x: len(x[1]), reverse=True)  # reverse the sort i.e. largest first
+# print groupby_artisti
+# out_file = open("../OUTPUT/top_artists_user.csv", "w")
 count = 1
-for key, item  in artist_count.iteritems():
-    newStr = str(count) + "," + key + "," + str(item)+"\n"
-    out_file.write(newStr)
-    count +=1
-out_file.close()
+count_unique = 0
+array_user = []
+for key, item in groupby_artisti:
+    unique_user = len(item['user_id'].unique())
+    newStr = str(count) + "," + key + "," + str(len(item))+"," + str(unique_user) + "\n"
+    # out_file.write(newStr)
+    if "florance" in key.lower():
+        print key
+        print unique_user
+        for index, row in item.iterrows():
+            array_user.append(row["user_id"])
+        count_unique += unique_user
+
+    # print key
+    # print item
+    # sys.exit()
+    count += 1
+# out_file.close()
+print len(array_user)
+print array_user
+np_array = np.array(array_user)
+unique_array = np.unique(np_array)
+print unique_array
+print count_unique
