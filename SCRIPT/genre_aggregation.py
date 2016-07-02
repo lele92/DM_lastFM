e5d43fb537ff6848dd7da0f037090451cae5707f
trappola_7 <matteo.borghi20@gmail.com>
2016-07-02 16:55:48 +0200

    - scripts for genre grouping

---
+++ SCRIPT/genre_aggregation.py
@@ -0,0 +1,49 @@
+__author__ = 'Trappola'
+
+import pandas as pd
+
+
+def main():
+
+    # 1 - subgenres to genres aggregation
+    genres_substitution("../OUTPUT/listenings_genre_merged.csv")
+    # genres_substitution("../OUTPUT/prova.csv")
+
+    # # loading dataset obtained
+    # df = pd.read_csv("listenings_subst_genres.csv", delimiter = ",", skipinitialspace = True)
+    #
+    # #2 - Saving datasets of the first five genres
+    # first_genres = ["rock", "pop", "electronic", "metal", "hip hop"]
+    # for item in first_genres:
+    #     extract_dataset(df, item)
+
+
+
+def extract_dataset(data, genre):
+    data = data[data["genre"] == genre]
+    data.to_csv(genre + ".csv", index=False)
+
+
+def genres_substitution(filename):
+    df = pd.read_csv(filename, skipinitialspace=True, delimiter=",")
+    print df.describe()
+    trovato = False
+    count = 0
+    # genres = ["indie rock", "rock", "pop", "metal", "electronic", "hip pop", "indie", "r&b", "punk", "folk", "jazz", "emo", "house", "soul"]
+    genres = ["indie rock", "rock", "pop", "metal", "electronic", "hip pop", "indie", "r&b", "punk", "folk"]
+    for index, item in df.iterrows():
+        trovato = False
+        for obj in genres:
+            if obj in item["genre"]:
+                df.set_value(index, "genre", obj)
+                trovato = True
+                break
+        if not trovato:
+            df.set_value(index, "genre", "altro")
+            count += 1
+    print count
+
+    df.to_csv("../OUTPUT/listenings_genre_merged_substitution_top10.csv", index=False)
+
+if __name__ == "__main__":
+    main()
---
+++ SCRIPT/genre_user_line_extraction.py
@@ -0,0 +1,44 @@
+__author__ = 'Trappola'
+
+import pandas as pd
+import datetime
+import sys
+import matplotlib.pyplot as plt
+from collections import Counter
+
+# genres_aggregator = ["indie rock", "rock", "pop", "metal", "electronic", "hip pop", "indie", "r&b", "punk", "folk", "jazz", "emo", "house", "soul", "altro"]
+genres_aggregator = ["indie rock", "rock", "pop", "metal", "electronic", "hip pop", "indie", "r&b", "punk", "folk", "altro"]
+
+
+def load_csv(input_filename):
+    return pd.read_csv("../OUTPUT/"+input_filename, skipinitialspace=True, delimiter=",", error_bad_lines=False)
+
+
+# df = load_csv("prova3.csv")
+# df = load_csv("listenings_genre_merged_substitution.csv")
+df = load_csv("listenings_genre_merged_substitution_top10.csv")
+print "######################### File Letto ##################################################################"
+
+out_file_user_listening = open("../OUTPUT/genre_listenings_user_quota_top10.csv", "w")
+# out_file_user_listening = open("../OUTPUT/genre_listenings_user_quota_top15.csv", "w")
+out_file_user_listening.write("user_id,num_ascolti_totali,indie rock,rock,pop,metal,electronic,hip pop,indie,r&b,punk,folk,jazz,emo,house,soul,altro\n")
+# out_file_user_listening.write("user_id,num_ascolti_totali,indie rock,rock,pop,metal,electronic,hip pop,indie,r&b,punk,folk,altro\n")
+
+df_grouped = df.groupby(['user_id'])
+
+for key, value in df_grouped:
+    generi = value["genre"].values.tolist()
+    counter = Counter(generi)
+
+    listenings_dict = {}
+    listenings_dict = dict(counter)
+    string_data = key + "," + str(len(generi))
+    for genre in genres_aggregator:
+        count = 0
+        if genre in listenings_dict:
+            count = listenings_dict[genre]
+        string_data += "," + ("{0:.2f}".format(float(count)/float(len(generi))))
+    out_file_user_listening.write(string_data+"\n")
+    out_file_user_listening.flush()
+    # sys.exit()
+out_file_user_listening.close()
