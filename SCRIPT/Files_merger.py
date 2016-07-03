81edfa7a937ccc8900285b3b58347821cf1f9062
lele <raph.giannella@gmail.com>
2016-07-03 14:46:03 +0200

    modifiche: - modifcati file churning

--- SCRIPT/Files_merger.py
+++ SCRIPT/Files_merger.py
@@ -5,7 +5,7 @@ def merge(df_left, df_right, left_on, right_on, out_file, columns_rename_obj=Non
     df_merged = pd.merge(df_left, df_right,
                             left_on=left_on,  #['user_id', 'week_year']
                             right_on=right_on,
-                            how='right')
+                            how='inner')
 
     if not columns_rename_obj == None:
         df_merged = df_merged.rename(columns=columns_rename_obj) #{'listening_count_x': 'total_count', 'listening_count_y': 'artist_count'}
@@ -22,10 +22,13 @@ def load_csv(input_path):
 
 churn_info_path = "../OUTPUT/muse/ascolti_utenti_costanti_muse_churning_15.csv"
 friends_info_path = "../OUTPUT/muse/users_listenings_with_friends.csv"
-out_path = "../OUTPUT/muse/test.csv"
+out_path_churn_friends = "../OUTPUT/muse/users_listenings_churn_friends.csv"
+
 
 df_churn_info = load_csv(churn_info_path)
 df_friends_info = load_csv(friends_info_path)
 
-merge(df_friends_info,df_churn_info,"user_id","id",out_path)
+
+merge(df_friends_info,df_churn_info,"user_id","user_id",out_path_churn_friends)
+merge(df_friends_info,df_churn_info,"user_id","user_id",out_path_churn_friends)
 
